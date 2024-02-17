18a95b37f5f5c1f39c9a381087fd422fc3c4f99f
jayeeed <jayedbinjahangir@gmail.com>
2024-02-18 03:40:25 +0600

    configs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable no-undef */
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+  // Load app-level env vars to node-level env vars.
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  // https://vitejs.dev/config/
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: Number(process.env.VITE_PORT),
+    },
+  });
+};
